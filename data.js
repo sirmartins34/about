3cf0e7fb67bdb1789d1e87d9f958b04391ec0cd6
SirMartins34 <sergiomartins1977@gmail.com>
2025-10-09 16:07:12 +0100

    Updates

---
+++ data.js
@@ -0,0 +1,231 @@
+﻿	var experience =
+        [
+			{
+				"row": 1, "local": "University - Faculdade de Ciências e Tecnologia", "abrev": "University",
+				"color": "#00a800", "hightlightBrightness": 0.2,
+				"init": 2000.01, "end": 2002.07, "textalign": "middle",
+				"projects": [
+					{
+						"description": "BSc (Hons) in Computer Engineering with: 2:1 degree (12.2 out of 20)\nCourse completion internship with: 1:1 degree (17 out of 20)"
+					}
+				]
+			},
+			{
+				"row": 3, "local": "at Introsys – Integração de Sistemas Robóticos, SA", "abrev": "Internship",
+				"color": "#a9a900", "hightlightBrightness": 0.2,
+				"init": 2001.04, "end": 2001.08, "textalign": "start",
+				"projects": [
+					{
+						"description": "Responsible for developing a PDA (Palm®) application: analysis, planning and application development to supervise in real time the status of assembly line robots.",
+						"tools": "Eclipse",
+						"languages": "Java J2SE 1.4, Java Beans"
+					}
+				]
+			},
+			{
+				"row": 2, "local": "at Portugal Telecom - Sistemas de Informação", "abrev": "Internship",
+				"color": "#23238E", "hightlightBrightness": 0.3,
+				"init": 2001.09, "end": 2002.07, "textalign": "end",
+				"projects": [
+					{
+						"description": "Development of a process to support call centres’ operators entering faults / issues reported by customers. Through a SharePoint website, other operators were tasked with analysing each reported issue and, according to its type, allocating the corresponding resources for its resolution. At the end of the process, those same operators were also tasked with progressing the issues’ status to resolved. The process also monitored time spent in resolution and launched alerts via email to the responsible parties involved. The entire process could involve the use of several different databases, depending on the type of fault / issue and resources to be considered.\nInternship course completion assignment grade: 17 in 20.",
+						"tools": "Microsoft Server BizTalk 2004, Microsoft Visual Studio .NET 2003, Microsoft SharePoint Portal 2003, Microsoft Office InfoPath 2003, Microsoft Internet Information Services 5.1",
+						"languages": "Dot.Net C# 1.1, Biztalk workflow, Biztalk inline C#"
+					}
+				]
+			},
+			{
+				"row": 1, "local": "Portugal Telecom - Sistemas de Informação", "abrev": "PTSI",
+				"color": "#23238E", "hightlightBrightness": 0.3,
+				"init": 2002.07, "end": 2005.12, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Implementation of a middleware for a nationwide network of shops. A walk-in customer could use this service to purchase numerous online products or make payments for services (which were typically paid at an Automated Teller Machine - ATMs). Upon completing the purchase, the respective connection service to the supplier company was triggered for each product, dealing with the specific and respective requirements and functionalities. Scenarios such as failure of the purchase or errors during payments were also covered. The middleware also handled / split the payout amongst all involved parties (shop, Entigere, PT and supplier companies).",
+						"tools": "Microsoft Server BizTalk 2004, Microsoft Visual Studio .NET 2003, Microsoft SQL Server 2000, Microsoft Internet Information Services 5.1",
+						"languages": "Dot.Net C# 2.0, Transact-SQL, Biztalk workflow, Biztalk inline C#",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "at Sport Lisboa e Benfica", "abrev": "Outsourcing",
+				"color": "#a90000", "hightlightBrightness": 0.2,
+				"init": 2003.06, "end": 2004.03, "textalign": "start",
+				"projects": [
+					{
+						"description": "Maintenance of a previously developed application for ticket sales’ management. Each ticket sold triggered a workflow process, resulting in the creation and / or updating of various database records with the objective of reserving or cancelling stadium seats. The system also integrated with the Primavera software (a financial management software), for financial purposes.",
+						"tools": "Microsoft Server BizTalk 2002, Microsoft Visual Studio .NET 2003, Microsoft SQL Server 2000, Microsoft Internet Information Services 5.1",
+						"languages": "Dot.Net C# 1.1, Transact-SQL",
+					},
+					{
+						"description": "Development of an intranet, where some employees could login with their credentials - and depending on their department, profile / role - they could access reports on ticket sales, or get quantity and distribution of members. On this intranet, a search members page was built for the Marketing team, with several filters, allowing them to send personalized promotional / informative emails. The members’ selection was obtained by a dynamic SQL query.",
+						"tools": "Microsoft Visual Studio .NET 2003, Microsoft SQL Server 2000, Microsoft Internet Information Services 5.1",
+						"languages": "Dot.Net C# 2.0, Transact-SQL, Javascript, HTML",
+					}
+				]
+			},
+            {
+				"row": 1, "local": "Sport Lisboa e Benfica", "abrev": "SLB",
+				"color": "#a90000", "hightlightBrightness": 0.2,
+				"init": 2005.12, "end": 2014.05, "textalign": "middle",
+				"projects": [
+					{
+						"description": "SIAD website – aggregated information from several departments in order to centralise and manage all club athletes’ data (i.e., data from all sports), speeding up and enhancing cooperation between the departments.\nDevelopment of a set of tools in Windows Forms to assist football strategists in collecting information regarding tactics and statistics. The results were exported to “SIAD” (previously mentioned).",
+						"tools": "Microsoft Visual Studio 2010 / 2008 / 2005, Microsoft SQL Server 2012 / 2008 / 2005 / 2000, Telerik ASP.NET AJAX Q1 2008-Q2 2012, Telerik ASP.NET Q3 2007, Telerik Reporting Q3 2007 SP1, Winnovative, Javascript library WalterZorn, SVN Source Control, Microsoft Source Safe, Microsoft Internet Information Services 7.5, Microsoft Internet Information Services 5.1, LINQPad 4.0",
+						"languages": "Dot.Net C# 3.5, Linq, Transact-SQL, Silverlight 5, JQuery, Javascript, HTML, WCF Services, Web Services, Windows Forms",
+					},
+					{
+						"description": "Delivery of a mobile ticketing service, providing the club’s members, a quick access to the Stadium during football matches via Short Message Service (SMS) with a 3D barcode or an URL link (Uniform Resource Locator) to a 3D barcode. This service provided ease of usage, faster entry, cost reduction in emission of physical tickets, enhanced security and overall customer experience. Before then, the only method to purchase a ticket was at the Stadium’s physical ticket office. SMS messages were created using multiple protocols: Enhanced Messaging Service (EMS), Smart Messaging (generated picture in OTA format) and Webpage Link (WAP Push)",
+						"tools": "Microsoft Visual Studio 2010/2008, Microsoft SQL Server 2012 / 2008 / 2005, Ozeki SMS Server, Neodynamic Barcode Professional 6.0, SVN Source Control, Microsoft Internet Information Services 7.5",
+						"languages": "Dot.Net C# 4.5, Transact-SQL, Windows Services, Web Services, Javascript, HTML",
+					},
+					{
+						"description": "Several other projects. Please, check my CV.",
+						"tools": "Microsoft Visual Studio 2003-2010, Telerik ASP.NET Q3 2007, Telerik ASP.NET AJAX Q3 2010-Q2 2012, Microsoft SQL Server 2000-2012, TUIO simulator (reacTIVision), Javascript library WalterZorn, SVN Source Control, Microsoft Source Safe, Microsoft Internet Information Services 5.1-7.5",
+						"languages": "Dot.Net C# 2.0-4.5, Transact-SQL, Javascript, JQuery, Linq, HTML, Dot.Net C# TUIO library, WPF, WCF Services",
+					}
+				]
+			},
+            {
+				"row": 1, "local": "IRIS Software Group Ldt.", "abrev": "IRIS",
+				"color": "#62B5E5", "hightlightBrightness": 1.9,
+				"init": 2014.07, "end": 2017.08, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Programming / Support of two major IRIS Products – IRIS OpenAccounts and IRIS OpenTax, using Agile / Scrum framework:\nDevelopment, code refactoring and maintenance of the aforementioned IRIS Products.\nCreation of tools to amend existing data, as well as, update functionalities, perform bug fixes and code improvements on the existing accounting tools to improve end users’ usability.\nOccasionally, assumed Team Leader’s responsibilities.\nAssisted the Quality Assurance (QA) team amending automated tests using Selenium and Gallio.",
+						"tools": "Microsoft Visual Studio 2015 / 2013 / 2012, Microsoft SQL Server 2013 / 2012, Telerik MVC Q1, Git, Windows Forms, Microsoft Internet Information Services 7.5, LINQPad 4.0, RabbitMQ, Agile / Scrum",
+						"languages": "Dot.Net C# 4.5, Linq, MVC, JQuery, Javascript, Transact-SQL, HTML, CSS, MSTest framework",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "Hack Day Winner 2014", "abrev": "🏆",
+				"color": "#EEA425", "hightlightBrightness": 1.2,
+				"init": 2014.12, "end": 2014.12, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Hack Day Winner 2014\n@IRIS Software Group Ltd, London (United Kingdom)",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "Hack Day Runner-Up 2015", "abrev": "🥈",
+				"color": "#EEA425", "hightlightBrightness": 1.2,
+				"init": 2015.04, "end": 2015.04, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Hack Day Runner-Up 2015\n@IRIS Software Group Ltd, London (United Kingdom)",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "Hack Day Winner 2016", "abrev": "🏆",
+				"color": "#EEA425", "hightlightBrightness": 1.2,
+				"init": 2016.12, "end": 2016.12, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Hack Day Winner 2016\n@IRIS Software Group Ltd, London (United Kingdom)",
+					}
+				]
+			},
+			{
+				"row": 1, "local": "Lawn Tennis Association", "abrev": "LTA",
+				"color": "#006AB6", "hightlightBrightness": 1.2,
+				"init": 2017.09, "end": 2022.10, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Multiple developments on a website that supports management areas for coaches, memberships and courses according to business requests, with integration with Siebel (Customer Relationship Management - CRM) and EpiServer components (Content Management System - CMS).\nCreation of a secure Payment Gateway for internal Stripe payments, to allow users to purchase services and products at all LTA websites. The gateway was later upgraded to comply with the Strong Customer Authentication directive (SCA / PSD2). More than two hundred-unit tests were added to the solution, and a new step was added to the Octopus Deploy process to run them, assuring that the right payment process was followed.\nDevelopment and improvement of Octopus Deploy processes (integrated with Team City) securing 100% of automated deployments and errors dealt. Deployment’s velocity was improved by 80% assuring a 100% availability of the website and a clean transition in terms of load balancing into a new version.\nImpact mitigation on existing LTA websites’ features and on the Payment Gateway after the migration of the existing CRM system. A brand-new LTA website was developed and coexisted with the existing websites. It was crucial for the login process to store the user’s session catering for smooth transitions and access to all websites’ features (among older and newer websites), as well as, synchronisation of the user’s membership and its benefits / access after a payment on a different website. This ensured a seamless end user experience regardless of the used website (older or newer).\nAutomation of the setup of new independent branches of LTA websites (onto Team City, Octopus Deploy, SQL Server and Web Server) via the creation of PowerShell scripts. These scripts enabled the team’s parallel development and testing, reducing the need for manual operations from almost 2 days to 10 minutes and also reducing the human error in 100%.\nFacilitate a seamless and transparent integration between Salesforce and JIRA via the development of a PowerShell script on the back-end code to call an API, that allowed the transfer of Support Tickets from Salesforce to JIRA.\nFirst steps at self-learning MuleSoft.",
+						"tools": "Microsoft Visual Studio 2020, Microsoft SQL Server 2016, Git, Microsoft Internet Information Services 7.5, Octopus Deploy, Team City, Stripe, EpiServer CMS 10, Postman, MuleSoft, Agile Scrum",
+						"languages": "Dot.Net C# 4.5 / 4.6, Linq, MVC, JQuery, Javascript, Angular, Transact-SQL, HTML, CSS, Rest API, Stripe API, Web services, Powershell script, MSTest framework",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "2nd Quarter Nomination 2018 (Passion)", "abrev": "🥈",
+				"color": "#EEA425", "hightlightBrightness": 1.2,
+				"init": 2018.04, "end": 2018.04, "textalign": "middle",
+				"projects": [
+					{
+						"description": "2nd Quarter Nomination 2018 (Passion)\n@LTA – Lawn Tennis Association, London (United Kingdom)",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "Hack Day Winner 2019", "abrev": "🏆",
+				"color": "#EEA425", "hightlightBrightness": 1.2,
+				"init": 2019.05, "end": 2019.05, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Hack Day Winner 2019\n@LTA – Lawn Tennis Association, London (United Kingdom)",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "2nd Quarter Winner 2020 (Passion)", "abrev": "🏆",
+				"color": "#EEA425", "hightlightBrightness": 1.2,
+				"init": 2020.04, "end": 2020.04, "textalign": "middle",
+				"projects": [
+					{
+						"description": "2nd Quarter Winner 2020 (Passion)\n@LTA – Lawn Tennis Association, London (United Kingdom)",
+					}
+				]
+			},
+            {
+				"row": 3, "local": "(Personal project)", "abrev": "👑",
+				"color": "#006AB6", "hightlightBrightness": 1.2,
+				"init": 2020.11, "end": 2021.05, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Mobile Android game '“King’s Valley'\nSelf-taught React JS and React Native, culminating on the development of a fully functional Android game. This 1st version was available on the Google Play Store – King’s Valley and it was deprecated by mid-March 2024.",
+						"tools": "Visual Code, Expo Go, npm, npx, Google Play Store, Android Studio, Paint.Net",
+						"languages": "React Native, Expo, Javascript, React StyleSheet, React JS, HTML, CSS",
+					}
+				]
+			},
+			{
+				"row": 4, "local": "(Personal project)@Google Play Store", "abrev": "👑",
+				"color": "#006AB6", "hightlightBrightness": 1.2,
+				"init": 2021.04, "end": 2024.04, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Release at Google Play Store – Mobile Android game King’s Valley\n@London (United Kingdom)\nThis 1st version was available on the Google Play Store – King’s Valley and it was deprecated by mid-March 2024",
+					}
+				]
+			},
+			{
+				"row": 2, "local": "Year Award Winner 2020 (Passion)", "abrev": "🏆",
+				"color": "#EEA425", "hightlightBrightness": 1.2,
+				"init": 2021.06, "end": 2021.06, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Year Award Winner 2020 (Passion)\n@LTA – Lawn Tennis Association, London (United Kingdom)",
+					}
+				]
+			},
+			{
+				"row": 1, "local": "Edge10", "abrev": "E10",
+				"color": "#0E1327", "hightlightBrightness": 0.3,
+				"init": 2022.11, "end": 2025.10, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Analysis, development and maintenance of the medical website that allows management of teams, players, their medical conditions, injuries and medication:\nAdoption of Kanban / Agile (Scrum) frameworks, aiming towards most valuable deliverables.\nSpearheaded context engineering initiatives across two client projects since January 2025. The implementation was done with Claude Code, Cline and Model Context Protocol (MCP), reducing development cycles for cross-functional delivery teams by over 50%.\nReduction of production incidents by 80% in one month through proactive bug fixing, aiming for a bug-free backlog.\nAddition of new or extending existing medical exams and their associated properties, adding configurable options according to client needs, implementing data migration, legacy code improvements, reporting, among other duties. Changing, updating and maintaining data import of multiple Application Programming Interfaces (APIs).\nAutomate weekly and monthly Excel reports’ generation.\nDevelopment of extendable multi-language support for improved end user experience, enhanced accessibility and wider audience reach. The feature also caters for customisation of the website’s texts.\nDevelopment of an application to generate T-SQL scripts, which sources data from Excel and configurable templates, to import hundreds / thousands of players, clubs, teams, venues, player positions, etc. It’s imperative to guarantee data integrity / quality in the database.\nExperienced on Azure DevOps CI / CD with Azure Pipelines aiming for efficiency / security gains, scalability, reliability and continuous integration",
+						"tools": "Microsoft Visual Studio 2022, Microsoft SQL Server 2019, Git, Microsoft Internet Information Services 10, Azure Pipelines / Releases, Postman, mRemoteNG, Jira, Azure DevOps, Agile, Kanban",
+						"contextEngineering": "Cline + MCP servers / Claude Code",
+						"languages": "Dot.Net C# Framework 4.5-4.8, Linq, MVC, JQuery, Javascript, Rest API, Transact-SQL, HTML, CSS",
+					}
+				]
+			},
+            {
+				"row": 3, "local": "(Personal project)", "abrev": "👑",
+				"color": "#006AB6", "hightlightBrightness": 1.2,
+				"init": 2025.09, "end": undefined, "textalign": "middle",
+				"projects": [
+					{
+						"description": "Mobile Android / iOS game “King’s Valley” (2nd version)\nUpgrade of the 1st version’s solution, with the latest packages versions, code refactoring and improvements, implementation of two new game expansions and addition of extra languages to the existing six languages. The goal is to have the Android and iOS versions available worldwide, once the development is complete.",
+						"tools": "Visual Code, Expo Go, npm, npx, Android Studio, Paint.Net",
+						"languages": "React Native, Expo, Javascript, React StyleSheet",
+					}
+				]
+			},
+        ]
+	;
+	
---
+++ static/js/488.725be7e8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkabout=self.webpackChunkabout||[]).push([[488],{488:(e,a,u)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.725be7e8.chunk.js.map
